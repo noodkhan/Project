572c8a8e36684a1dbf63ba5b784d2345369dcfc4
Noodkhan_404 <92358053+noodkhan@users.noreply.github.com>
2022-05-06 08:11:43 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import FirstPage from './FirstPage/FirstPage';
+import SecondPage from './SecondPage/SecondPage';
+import ThridPage from './ThirdPage/ThridPage';
+import './Logic.js';
+import ForthPage from './ForthPage/ForthPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FirstPage></FirstPage>
+      <SecondPage></SecondPage>
+      <ThridPage></ThridPage>
+      <ForthPage></ForthPage>
     </div>
   );
 }
